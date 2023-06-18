c804734c7878d1d1d3182a764b3b209ec758beb9
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-06-18 19:08:37 -0400

    configure routes

--- server/controllers/entries.js
+++ server/controllers/entries.js
@@ -1,22 +1,52 @@
-const getAllEntries = (req, res) => {
-    res.send('All entries')
-}
+const Entry = require('../models/Entry')
+const asyncWrapper = require('../middleware/async')
 
-const getEntry = (req, res) => {
-    res.send('single entry')
-}
+const getAllEntries = asyncWrapper( async(req, res) => {
+    const allEntries = await Entry.find({})
+    res.status(200).json({ allEntries })
+})
 
-const createEntry = (req, res) => {
-    res.json(req.body)
-}
+const getEntry = asyncWrapper( async(req, res) => {
+    const {id} = req.params
+    const entry = await Entry.findOne({_id:id})
 
-const updateEntry = (req, res) => {
-    res.send('update entry')
-}
+    if(!entry){
+        return res.status(404).json({message: `No entry with id: ${id}`})
+    }
 
-const deleteEntry = (req, res) => {
-    res.send('delete an entry')
-}
+    res.status(200).json({ entry })
+})
+
+const createEntry = asyncWrapper ( async(req, res) => {
+    const entry = await Entry.create(req.body)
+    res.status(201).json({ entry })
+})
+
+const updateEntry = asyncWrapper( async(req, res) => {
+    const {id} = req.params
+    const entry = await Entry.findByIdAndUpdate({_id: id}, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    if(!entry){
+        return res.status(404).json({message: `No entry with id: ${id}`})
+    }
+
+    res.status(200).json({ entry })
+
+})
+
+const deleteEntry = asyncWrapper( async(req, res) => {
+    const {id} = req.params
+    const entry = await Entry.findOneAndDelete({_id:id})
+
+    if(!entry){
+        return res.status(404).json({message: `No entry with id: ${id}`})
+    }
+
+    res.status(200).json({ success: 'true' })
+})
 
 module.exports = {
     getAllEntries, 
