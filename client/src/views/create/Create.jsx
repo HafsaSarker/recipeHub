ff3a21c0e24839fa5d18c91aa275312c89dfa5c6
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-06-21 13:56:13 -0400

    add create-post form

--- client/src/views/create/Create.jsx
+++ client/src/views/create/Create.jsx
@@ -1,9 +1,58 @@
+import { useState } from 'react'
 import './Create.css'
 
 export default function Create() {
+    const [newPost, setNewPost] = useState({
+        name: '',
+        imgUrl: '',
+        recipeLink: ''
+    })
+
+    function handleChange(e){
+        const {name, value} = e.target 
+
+        setNewPost((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+    
+    function createPost(e){
+        e.preventDefault()
+        console.log(newPost)
+    }
     return (
-        <div className="create-page">
-            Create
+        <div className="create-page" onSubmit={createPost}>
+            <form className='create-post-form'>
+                <label>
+                    recipe title:
+                    <input 
+                        type="text"
+                        name='name'
+                        required
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    image url:
+                    <input 
+                        type="text"
+                        name='imgUrl'
+                        required
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    recipe link:
+                    <input 
+                        type="text"
+                        name='recipeLink'
+                        required
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
