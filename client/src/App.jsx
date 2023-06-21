67d90397ae2ad3c042194acf0c0e2e069b05e404
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-06-21 13:37:41 -0400

    set up nav links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./views/home/Home";
 import './App.css'
+import Create from "./views/create/Create";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
+          <Route path="/create" element={<Create />}/>
         </Route>
       </Routes>
     </div>
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import './Nav.css'
 
 export default function Nav() {
     return (
         <div className="nav">
             <ul>
-                <li>Home</li>
-                <li>Create</li>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                
+                <Link to="/create">
+                    <li>Create</li>
+                </Link>
             </ul>
         </div>
     )
