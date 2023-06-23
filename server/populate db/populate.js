4c7d39cd6398061c2cc0f6bb3686ed9184a447db
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-06-23 17:35:45 -0400

    populate db with json file

---
+++ server/populate db/populate.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const connectDB = require('../db/connect')
+const Entries = require('../models/Entry')
+
+const json = require('./entries.json')
+
+const populateDB = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        await Entries.deleteMany()
+        await Entries.create(json)
+
+        console.log('All products inserted. Exiting...')
+
+        process.exit(0)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+populateDB()
